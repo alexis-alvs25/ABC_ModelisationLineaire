310acc8ca83c7dff23f974892e6b9e91d3957002
ALVES Alexis <alexis.alves@etu.uca.fr>
2025-01-28 15:57:47 +0100

    Initial commit

---
+++ TP1Functions.c
@@ -0,0 +1,52 @@
+#include "TP1Functions.h"
+#include <math.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/time.h>
+#include<stdio.h>
+
+int read_TP1_instance(FILE*fin,dataSet* dsptr)
+{
+	int rval = 0;
+
+	//capacite b
+	int b;
+	//Nombre d'objets
+	int n;
+	rval = fscanf(fin,"%d,%d\n",&n,&b);
+	dsptr->b = b;
+	dsptr->n = n;
+	dsptr->c = (int*)malloc(sizeof(int)*n);
+	dsptr->a = (int*)malloc(sizeof(int)*n);
+
+
+	int i;
+	for( i = 0 ; i < n ; i++)
+		rval = fscanf(fin,"%d,%d\n",&(dsptr->c[i]),&(dsptr->a[i]));
+
+	fprintf(stderr,"\nInstance file read, we have capacity %d and there is %d items of values/weights:\n",
+			b,n);
+	for( i = 0 ; i < n ; i++)
+		fprintf(stderr,"%d,%d\n",dsptr->c[i],dsptr->a[i]);
+	fprintf(stderr,"\n");
+
+
+	return rval;
+}
+
+int KP_greedy(dataSet* dsptr)
+{
+	int rval = 0;
+
+
+	return rval;
+}
+
+int KP_LP(dataSet* dsptr)
+{
+	int rval = 0;
+
+
+	return rval;
+}
+
