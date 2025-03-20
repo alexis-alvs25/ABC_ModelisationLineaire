74bfeb967cb38ed9b7492e8c18166a37bedddd34
Fayolle Bastien <fayollebastien@gmail.com>
2025-03-20 14:13:38 +0100

    l'erreur de nullos quoi (en float, pas int)

--- TP3/TP3Functions.c
+++ TP3/TP3Functions.c
@@ -6,12 +6,13 @@
 #include "../TP1/TP1Functions.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-solution * withPreprocessing(dataSet *data)
+solution * preprocessing(dataSet *data)
 {
     solution* sol_greed  = KP_greedy(data);
     solution * ideal = KP_LP(data);
-    int j,p,sum, optval ;
+    int j,p;
+    float optval;
+
     printf("L'algorithme glouton donne : %f\n, la relaxation linéaire : %f\n", sol_greed->value, ideal->value);
     p=0;
     int numberOfObject = 0;
@@ -26,12 +27,11 @@ solution * withPreprocessing(dataSet *data)
 
 
     for(j=0; j<data->n; j++){
-        optval=fabs(data->c[j]-(((float)data->c[p]/(float)data->a[p])*(float)data->a[j]));
+        optval=fabs((float)data->c[j]-(((float)data->c[p]/(float)data->a[p])*(float)data->a[j]));
         recalcSol(sol_greed,data);
-        printf("optval : %d, compared to %f \n",optval, fabs(ideal->value -sol_greed->value ));
-
+        printf("optval : %f, compared to %f \n",optval, fabs(ideal->value -sol_greed->value ));
         if(optval>=ideal->value -sol_greed->value ){
-          if(j<p-1){
+          if(j<=p-1){
             printf("1\n");
             sol_greed->xbar[j]=1;
             fixed0[j]=0;
